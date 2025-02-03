2b5ead0b297a21c539df2e76cf132c77e37f7399
Siddhartha <sidd.patty@gmail.com>
2025-02-03 22:58:50 +0530

    done for the time being

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+
+
 from flask import Flask,render_template,request
 from flask_socketio import SocketIO, emit
 import random
@@ -12,28 +14,47 @@ users = {}
 def index():
     return render_template('index.html')
 
+
 @socketio.on("connect")
 def handle_connect():
-    username = f"User_{random.randint(1000, 999)}"
-    gender = random.choice(["girl", "boy"])
-    avatar_url = f"https://avatar.iran.liara.run/public/{gender}?username={username}"
+    username = f"User_{random.randint(1000,9999)}"
+    gender = random.choice(["girl","boy"])
+   
+    avatar_url = f" https://avatar.iran.liara.run/public/{gender}?username={username}"
 
-    users[request.sid] = {"username": username, "avatar": avatar_url}
+    users[request.sid] = { "username":username,"avatar":avatar_url}
 
+    emit("user_joined", {"username":username,"avatar":avatar_url},broadcast=True)
 
-    emit("user_joined", {"username":username, "avatar":avatar_url}, broadcast=True)
     emit("set_username", {"username":username})
 
 @socketio.on("disconnect")
 def handle_disconnect():
     user = users.pop(request.sid, None)
     if user:
-        emit("user_left", {"username":user["username"]}, broadcast=True)   
+      emit("user_left", {"username":user["username"]},broadcast=True)
 
 
-@socketio.on("send message")
+@socketio.on("send_message")
 def handle_message(data):
-    pass
+    user = users.get(request.sid)
+    if user:
+        emit("new_message", {
+            "username":user["username"],
+            "avatar":user["avatar"],
+            "message":data["message"]
+        }, broadcast=True)
+
+@socketio.on("update_username")
+def handle_update_username(data):
+    old_username = users[request.sid]["username"]
+    new_username = data["username"]
+    users[request.sid]["username"] = new_username
+
+    emit("username_updated", {
+        "old_username":old_username,
+        "new_username":new_username
+    }, broadcast=True)
 
 if __name__ == "__main__":
-    socketio.run(app)
+    socketio.run(app) 
